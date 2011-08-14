1fc1e21f837a281d4b9706f0f0b26fcfb9145bfd
Tao-Sheng <taosheng.chen@gmail.com>
2011-08-14 16:36:04 +0800

    simple insert of Talent

--- itTalent/src/java/net/radzebra/itTalent/TalentManager.java
+++ itTalent/src/java/net/radzebra/itTalent/TalentManager.java
@@ -1,6 +1,16 @@
 package net.radzebra.itTalent ;
+import net.radzebra.itTalent.dao.* ;
 
 import java.util.*;
+import java.io.*;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 /**
 * A simple Talent database to record group members' story.
 * It based on sqlite and iBatis
@@ -23,6 +33,24 @@ public class TalentManager{
         println("demo the Talent class");
         TalentManager tm = new TalentManager();
         tm.addTalent("newtel");
+
+        String resource = "mybatisConfig.xml"; 
+        Reader reader = Resources.getResourceAsReader(resource); 
+        SqlSession sqlSession = new SqlSessionFactoryBuilder().build(reader).openSession();
+
+        sqlSession.getConnection().setAutoCommit(true);
+        
+        TalentMapper talentMapper = sqlSession.getMapper(TalentMapper.class);
+        Talent t = new Talent();
+        t.setId("0000001");
+        t.setName("radZebra");
+        t.setCellPhone("02-3939889");
+        t.setEmail("radZebra@gmail.com") ;
+        t.setBirthYear("2011");
+        t.setDesc("a default user of talent manager");
+
+        talentMapper.insert(t);
+        sqlSession.close();
     }
 
 
